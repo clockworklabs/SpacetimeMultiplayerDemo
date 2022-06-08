f1c33eb85cbf1fb26aaf615975c5b256f477b99a
Boppy <no-reply@boppygames.gg>
2022-06-08 07:37:34 -0500

    Nuget installed

--- Client/Assets/_Project/Player/CameraController.cs
+++ Client/Assets/_Project/Player/CameraController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpacetimeDBConnection
+public class CameraController : MonoBehaviour
 {
-    // public void 
+    
 }
